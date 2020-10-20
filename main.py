d10d4d29a18cd892707d14c2c43b0454af95af79
The Blue Chalk <thebluechalk@gmail.com>
2020-10-20 13:44:31 +0530

    training data conversion

--- main.py
+++ main.py
@@ -13,15 +13,48 @@ with open('intent.json') as file:
 
 words = []
 labels = []
-docs = []
+docs_x = []
+docs_y = []
 
 for intent in data["intents"]:
     for pattern in intent["patterns"]:
         wrds = nltk.word_tokenize(pattern)
         words.extend(wrds)
-        docs.append(pattern)
+        docs_x.append(pattern)
+        docs_y.append(intent["tag"])
         
         if intent["tag"] not in labels:
             labels.append(intent["tag"])
         
+words = [stemmer.stem(w.lower()) for w in words]
+words = sorted(list(set(words)))
+
+labels = sorted(labels)
+
+training = []
+output = []
+out_empty = [0 for _ in range(len(labels))]
+
+for x, doc in enumerate(docs_x):
+    bag = []
+
+    wrds = [stemmer.stem(w) for w in doc]
+
+    for w in words:
+        if w in wrds:
+            bad.append(1)
+        else:
+            bag.append(0)
+
+    output_row = outempty[:]
+    output_row[labels.index(docs_y[x])] = 1
+
+    training.append(bag)
+    output.append(output_row)
+
+training = np.array(training)
+output = np.array(output)
+
+
+
 
